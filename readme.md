b2f819ff9c20fea8ae1ae4d98e59bf7c86948019
Akash <codesecoder@gmail.com>
2025-06-15 00:01:54 +0530

    update karo

---
+++ readme.md
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -am""
+git remote add origin url
+git remote -v
+git push origin master
---
+++ script.js
@@ -0,0 +1,7 @@
+ function changeColor() {
+    const boxs = document.querySelectorAll('.topic1');
+    boxs.forEach(topic1 => {
+    topic1.classList.remove('bg-indigo-300');
+    topic1.classList.add('bg-red-500');
+  });
+}
